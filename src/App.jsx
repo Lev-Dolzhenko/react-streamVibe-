9bf087ff1c1e4fcc75da52f3a29a6be61b7458e4
Leo <workspace2707@yandex.ru>
2024-04-10 14:16:33 +0300

    created react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
+
+//pages
 import HomePage from "./pages/HomePage";
 import MoviesShowsPage from "./pages/MoviesShowsPage";
+import SupportPage from "./pages/SupportPage";
+import SubscriptionPage from "./pages/SubscriptionsPage";
+
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      <MoviesShowsPage />
+      <Routes>
+        <Route path="*" element={<HomePage to="/" />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/MoviesShowsPage" element={<MoviesShowsPage />} />
+        <Route path="/SupportPage" element={<SupportPage />} />
+        <Route path="/SubscriptionsPage" element={<SubscriptionPage />} />
+      </Routes>
     </>
   );
 }
--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 
+//react rounter dom
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 const HeaderNav = () => {
+  const nameLinks = [
+    "/",
+    "/MoviesShowsPage",
+    "/SupportPage",
+    "/SubscriptionsPage",
+  ];
   const nameButtons = ["Home", "Movies & Shows", "Support", "Subscriptions"];
   return (
     <nav className="header__nav">
       <ul className="header__list">
-        {nameButtons.map((nameButton) => (
-          <li key={nameButton} className="header__elem">
-            <button className="header__button" href="#!">
-              {nameButton}
-            </button>
-          </li>
+        {nameButtons.map((nameButton, index) => (
+          <Link key={nameButton} to={nameLinks[index]}>
+            <li className="header__elem">
+              <button className="header__button" href="#!">
+                {nameButton}
+              </button>
+            </li>
+          </Link>
         ))}
       </ul>
     </nav>
